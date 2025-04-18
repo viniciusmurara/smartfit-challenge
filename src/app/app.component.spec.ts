9cce9f7f4b1f8c8d325cdb55ada01072759dc13b
Vinicius Murara <viniciusvieiramurara@gmail.com>
2025-04-18 09:58:34 -0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, HeaderComponent],
     }).compileComponents();
   });
 
