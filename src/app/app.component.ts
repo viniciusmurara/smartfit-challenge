a0ae6e61ab27b293cba1e185568c25c980783351
Vinicius Murara <viniciusvieiramurara@gmail.com>
2025-04-19 09:57:20 -0300

    começando componente de formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { FormsComponent } from "./components/forms/forms.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FormsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
