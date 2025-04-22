3add7e69b695b84ec8b33452a947281085ca8686
Vinicius Murara <viniciusvieiramurara@gmail.com>
2025-04-22 10:17:24 -0300

    iniciando a parte funcional do formulário

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
 export class FormsComponent {
   results: string[] = [];
+  formGroup!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
+  }
 }
